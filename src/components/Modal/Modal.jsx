1ad97c7c95b792f82365a23671a28320dfe05944
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-05-10 19:06:18 +0200

    project setup

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,6 @@
+// import s from './Modal.module.scss';
+const Modal = () => {
+  return <div>Modal common</div>;
+};
+
+export default Modal;
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,6 @@
+// import s from "./Table.module.scss";
+
+const Table = () => {
+  return <div>Table</div>;
+};
+export default Table;
