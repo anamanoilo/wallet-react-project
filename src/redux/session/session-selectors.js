3885b170836042861a154db21ee41faa1ed8d494
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-05-10 22:45:51 +0200

    install chart.js

--- src/redux/session/session-selectors.js
+++ src/redux/session/session-selectors.js
@@ -1,5 +1,6 @@
 const getIsAuth = (state) => state.session.isAuth;
+const getUsername = (state) => state.session.user.name;
 
-const authSelectors = { getIsAuth };
+const authSelectors = { getIsAuth, getUsername };
 
 export default authSelectors;
