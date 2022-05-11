4c3ed2ffddcdcd418f592575b94d1e88a1bcf695
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-05-11 10:09:23 +0200

    fix fonts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from "react";
-import Header from "components/Header";
 import "stylesheet/shared.scss";
 
 function App() {
-  return (
-    <div>
-      App
-      <Header />
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
