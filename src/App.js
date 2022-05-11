673f338cb4c26a1aa5115cf14e4ceacdf13e42d1
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-05-11 10:08:10 +0200

    bugfix/fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "components/Header";
+import "stylesheet/shared.scss";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <Header />
+    </div>
+  );
 }
 
 export default App;
