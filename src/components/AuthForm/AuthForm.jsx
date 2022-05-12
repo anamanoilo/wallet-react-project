aabc80e4c002656179e1280662ab7a3203e47d63
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-05-12 09:52:50 +0300

    style

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,8 +1,10 @@
-// import s from "./AuthForm.module.scss";
+import s from "./AuthForm.module.scss";
 import { Formik, ErrorMessage } from "formik";
 import { validationSchema } from "./validationAuthForm";
 import { logIn, register } from "redux/session/auth-operation";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import Logo from "./";
 
 export const AuthForm = ({ type }) => {
   const dispatch = useDispatch();
@@ -35,106 +37,123 @@ export const AuthForm = ({ type }) => {
           isValid,
           dirty,
         }) => (
-          <form onSubmit={handleSubmit}>
-            {type === "signUp" ? (
-              <>
-                <label htmlFor="email">
-                  email
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="your@email.com"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                  />
-                  {/* {errors.email && touched.email && errors.email} */}
-                  <ErrorMessage component="div" name="email" />
-                </label>
+          <div className={s.containerForm}>
+            <form className={s.form} onSubmit={handleSubmit}>
+              {type === "signUp" ? (
+                <>
+                  <div>
+                    {/* <img className={s.logo} src={Logo} alt="logo" /> */}
+                    <h1 className={s.title}> Wallet</h1>
+                  </div>
+                  <div>
+                    <label className={s.formInput} htmlFor="email">
+                    E-mail
+                      <input
+                        className={s.formInput}
+                        type="email"
+                        name="email"
+                        placeholder="E-mail"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                      />
+                      {/* {errors.email && touched.email && errors.email} */}
+                      <ErrorMessage component="div" name="email" />
+                    </label>
+                  </div>
+                  <div>
+                    <label htmlFor="password">
+                      <input
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                      />
+                      {/* {errors.password && touched.password && errors.password} */}
+                      <ErrorMessage component="div" name="password" />
+                    </label>
+                  </div>
+                  <div>
+                    <label htmlFor="confirmPassword">
+                      <input
+                        type="password"
+                        placeholder="confirmPassword"
+                        name="confirmPassword"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.confirmPassword}
+                      />
+                      {/* {errors.password && touched.password && errors.password} */}
+                      <ErrorMessage component="div" name="confirmPassword" />
+                    </label>
+                  </div>
+                  <div>
+                    <label htmlFor="username">
+                      <input
+                        type="name"
+                        placeholder="your name"
+                        name="username"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.name}
+                      />
+                      {/* {errors.email && touched.email && errors.email} */}
+                      <ErrorMessage component="div" name="username" />
+                    </label>
+                  </div>
 
-                <label htmlFor="password">
-                  password
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                  />
-                  {/* {errors.password && touched.password && errors.password} */}
-                  <ErrorMessage component="div" name="password" />
-                </label>
-                <label htmlFor="confirmPassword">
-                  confirmPassword
-                  <input
-                    type="password"
-                    name="confirmPassword"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.confirmPassword}
-                  />
-                  {/* {errors.password && touched.password && errors.password} */}
-                  <ErrorMessage component="div" name="confirmPassword" />
-                </label>
-                <label htmlFor="username">
-                  Name
-                  <input
-                    type="name"
-                    name="username"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.name}
-                  />
-                  {/* {errors.email && touched.email && errors.email} */}
-                  <ErrorMessage component="div" name="username" />
-                </label>
-
-                <button
-                  type="submit"
-                  disabled={isSubmitting || !isValid || !dirty}
-                >
-                  registration
-                </button>
-              </>
-            ) : (
-              <>
-                <label htmlFor="email">
-                  email
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="your@email.com"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                  />
-                  {/* {errors.email && touched.email && errors.email} */}
-                  <ErrorMessage component="div" name="email" />
-                </label>
-
-                <label htmlFor="password">
-                  password
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                  />
-                  {/* {errors.password && touched.password && errors.password} */}
-                  <ErrorMessage component="div" name="password" />
-                </label>
-                <button
-                  type="submit"
-                  disabled={isSubmitting || !isValid || !dirty}
-                >
-                  LogIn
-                </button>
-              </>
-            )}
-          </form>
+                  <button
+                    type="submit"
+                    disabled={isSubmitting || !isValid || !dirty}
+                  >
+                    registration
+                  </button>
+                  <Link to="/login">login</Link>
+                </>
+              ) : (
+                <>
+                  <h1>Wallet</h1>
+                  <div>
+                    <label htmlFor="email">
+                      <input
+                        type="email"
+                        name="email"
+                        placeholder="your@email.com"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                      />
+                      {/* {errors.email && touched.email && errors.email} */}
+                      <ErrorMessage component="div" name="email" />
+                    </label>
+                  </div>
+                  <div>
+                    <label htmlFor="password">
+                      <input
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                      />
+                      {/* {errors.password && touched.password && errors.password} */}
+                      <ErrorMessage component="div" name="password" />
+                    </label>
+                  </div>
+                  <button
+                    type="submit"
+                    disabled={isSubmitting || !isValid || !dirty}
+                  >
+                    LogIn
+                  </button>
+                  <Link to="/signUp">signUp</Link>
+                </>
+              )}
+            </form>
+          </div>
         )}
       </Formik>
     </div>
