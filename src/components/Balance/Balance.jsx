9f96750a3fca298a23e15f0a94ddee8a30a99466
yurirusyn <iirusyn@gmail.com>
2022-05-14 09:33:48 +0200

    fix Balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,14 +1,25 @@
-import s from "./Balance.module.scss"
-import { useSelector } from 'react-redux';
-import financeSelectors from 'redux/finance/finance-selectors';
+import s from "./Balance.module.scss";
+import { useSelector } from "react-redux";
+import financeSelectors from "redux/finance/finance-selectors";
 
 const Balance = () => {
   const totalBalance = useSelector(financeSelectors.getTotalBalance);
+  function total(totalBalance) {
+    if (Number.isInteger(totalBalance)) {
+      totalBalance.toString();
+      return totalBalance.toString().concat(".00");
+    } else {
+      return totalBalance;
+    }
+  }
+  let sum = total(totalBalance);
+
   return (
     <div className={s.balance}>
       <p className={s.balanceTitle}>Your balance</p>
       <p className={s.balanceValue}>
-        <span className={s.symbol}>&#8372;</span>{totalBalance}
+        <span className={s.symbol}>&#8372;</span>
+        {sum}
       </p>
     </div>
   );
