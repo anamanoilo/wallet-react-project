bde8e3e1d7b312aa51f333e37e21500d5a35ee4b
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-05-14 20:35:25 +0300

    png

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,39 @@
 import s from "../Registration/registration.module.scss";
-import imgLogin from "../../assets/images/imgLogin.png";
+import imgLoginD1 from "../Registration/img/login-desktop-1x.png";
+import imgLoginD2 from "../Registration/img/login-desktop-2x.png";
+import imgLoginT1 from "../Registration/img/login-tablet-x1.png";
+import imgLoginT2 from "../Registration/img/login-tablet-x2.png";
 import sContainer from "../../components/AuthForm/ContainerForm.module.scss";
 import AuthForm from "../../components/AuthForm/AuthForm";
 const Login = () => {
+  const screenWidth = window.screen.width;
+  function screen(screenWidth) {
+    if (screenWidth > 1280) {
+      return (
+        <img
+          className={s.img}
+          src={imgLoginD1}
+          srcSet={`${imgLoginD2} 2x`}
+          alt="signup"
+        />
+      );
+    } else {
+      return (
+        <img
+          className={s.img}
+          src={imgLoginT1}
+          srcSet={`${imgLoginT2} 2x`}
+          alt="signup"
+        />
+      );
+    }
+  }
   return (
     <div className={s.registerBg}>
       <div className={sContainer.container}>
         <div className={s.mainPage}>
           <div className={s.left}>
-            <img src={imgLogin} alt="finance app" className={s.img} />
+            {screen(screenWidth)}
             <h1 className={s.title}>Finance App</h1>
           </div>
           <div className={s.right}>
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,17 +1,42 @@
 import AuthForm from "../../components/AuthForm/AuthForm";
-import imgRegister from "../../assets/images/imgRegister.png";
+import imgRegister from "./img/registration-desktop-1x.png";
+import imgRegister2x from "./img/registration-desktop-2x.png";
+import imgRegisterTablet from "./img/registration-tablet-1x.png";
+import imgRegisterTablet2x from "./img/registrartion-tablet-2x.png";
 import s from "./registration.module.scss";
 import sContainer from "../../components/AuthForm/ContainerForm.module.scss";
 import "react-toastify/dist/ReactToastify.min.css";
 
 const Registration = () => {
+  const screenWidth = window.screen.width;
+  function screen(screenWidth) {
+    if (screenWidth > 1280) {
+      return (
+        <img
+          className={s.img}
+          src={imgRegister}
+          srcSet={`${imgRegister2x} 2x`}
+          alt="signup"
+        />
+      );
+    } else {
+      return (
+        <img
+          className={s.img}
+          src={imgRegisterTablet}
+          srcSet={`${imgRegisterTablet2x} 2x`}
+          alt="signup"
+        />
+      );
+    }
+  }
+  console.log("screenWidth", screenWidth);
   return (
     <div className={s.registerBg}>
       <div className={sContainer.container}>
         <div className={s.mainPage}>
           <div className={s.left}>
-            <img src={imgRegister} alt="finance app" className={s.img} />
-            <h1 className={s.title}>Finance App</h1>
+            {screen()};<h1 className={s.title}>Finance App</h1>
           </div>
           <div className={s.right}>
             <AuthForm type="signUp" />
