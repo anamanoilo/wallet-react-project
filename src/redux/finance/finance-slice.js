58da8816a2dac99ae6e2ddb3401e3a3b3af7fa77
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-05-15 20:16:31 +0300

    boilerplate

--- src/redux/finance/finance-slice.js
+++ src/redux/finance/finance-slice.js
@@ -21,44 +21,86 @@ const financeSlice = createSlice({
   name: "finance",
   initialState,
   extraReducers: {
+    [allTransactions.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
     [allTransactions.fulfilled]: (state, { payload }) => {
+      state.loading = false;
       state.data = payload;
     },
     [allTransactions.rejected]: (state, { payload }) => {
+      state.loading = false;
       state.error = payload;
     },
     [getSummary.pending]: (state) => {
       state.loading = true;
+      state.error = null;
     },
     [getSummary.fulfilled]: (state, { payload }) => {
+      state.loading = false;
       state.summary = payload;
-      state.loading = false;
     },
-    [getSummary.rejected]: (state) => {
+    [getSummary.rejected]: (state, { payload }) => {
       state.loading = false;
+      state.error = payload;
     },
 
+    [getCategories.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
     [getCategories.fulfilled]: (state, { payload }) => {
+      state.loading = false;
       state.categories = payload.map((obj, i) => {
         return obj.type === "EXPANSE"
           ? { ...obj, backgroundColor: colors[i] }
           : obj;
       });
     },
+    [getCategories.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    [addTransaction.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
     [addTransaction.fulfilled]: (state, { payload }) => {
+      state.loading = false;
       state.data = [...state.data, payload];
     },
+    [addTransaction.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    [logOut.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
     [logOut.fulfilled]: (state) => {
+      state.loading = false;
       state.data = null;
       state.totalBalance = null;
       state.summary = null;
       state.error = null;
       state.categories = null;
     },
+    [logOut.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+
+    [refresh.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
     [refresh.fulfilled]: (state, { payload }) => {
+      state.loading = false;
       state.totalBalance = payload.balance;
     },
     [refresh.rejected]: (state, { payload }) => {
+      state.loading = false;
       state.error = payload;
     },
   },
