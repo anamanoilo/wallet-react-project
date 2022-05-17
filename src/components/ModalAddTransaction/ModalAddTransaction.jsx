88c889d836118c1a1f16427e2165a314d5f23a2e
andrpoch <andrpoch25@gmail.com>
2022-05-17 16:10:13 +0300

    fix replace

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -20,17 +20,13 @@ import { toast } from "react-toastify";
       if (!value || Number.isNaN(Number(value))) return value;
     const length = value.length;
     const dot = value.indexOf(".");
-
-    if (dot < 0) {
-      return value.replace('.', ',');
-    }
     if (dot < 0) {
       return value.concat(".00");
     }
     if (dot < length - 3) {
       return value.slice(0, dot + 3);
     }
-
+    
     if (dot > length - 3) {
       return value.padEnd(dot + 3, "0");
     }
